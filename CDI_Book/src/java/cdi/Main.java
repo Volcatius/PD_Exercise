ad01a2db093b1009aa09e493f11b843c325d7b20
Volcatius <andrea.carpentiere@hotmail.it>
2018-01-15 10:35:30 +0100

    Creato Main con libreria Weld-se-2.3.3

---
+++ CDI_Book/src/java/cdi/Main.java
@@ -0,0 +1,25 @@
+package cdi;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+/**
+ *
+ * @author andre
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Weld weld = new Weld();
+        WeldContainer container = weld.initialize();
+        
+        BookService bookService = container.instance().select(BookService.class).get();
+        
+        Book book = bookService.createBook("Grinfie", 5.2F, "Gigi non fare il push");
+        
+        System.out.println(book);
+        
+        weld.shutdown();
+    }
+    
+}

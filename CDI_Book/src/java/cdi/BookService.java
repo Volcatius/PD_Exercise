9f0ae06c20180630dc78569f85be31efce48539c
Volcatius <andrea.carpentiere@hotmail.it>
2018-01-15 14:58:14 +0100

    Inserito metodo alternativo con HashMap per loggable

--- CDI_Book/src/java/cdi/BookService.java
+++ CDI_Book/src/java/cdi/BookService.java
@@ -10,7 +10,7 @@ public class BookService {
     
     @Inject @ThirteenDigits
     private NumberGeneretor numberGeneretor;
-
+    
     public Book createBook(String title, float prize, String description){
         Book book = new Book(title, prize, description);
         book.setIsbn(numberGeneretor.generatedNumber());
--- CDI_Book/src/java/cdi/LoggingInterceptor.java
+++ CDI_Book/src/java/cdi/LoggingInterceptor.java
@@ -1,4 +1,5 @@
 package cdi;
+import java.util.HashMap;
 import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
@@ -29,4 +30,31 @@ public class LoggingInterceptor {
            log.exiting(ivc.getTarget().getClass().getName(), ivc.getMethod().getName());
         }
     }
+    
+    /*************************************************************************** 
+    **********Metodo Alternativo con HashMap**********
+    
+    private HashMap<String, Integer> listaMetodi = new HashMap<>();
+    
+    @AroundInvoke//ad ogni invocazione di un metodo
+    public Object logMethod(InvocationContext ic) throws Exception {
+        String nomeMetodo;
+        int count;
+        nomeMetodo = ic.getMethod().getName();
+        if(listaMetodi.containsKey(nomeMetodo)){
+            count = listaMetodi.get(nomeMetodo);
+            count++;
+            listaMetodi.put(nomeMetodo, count);
+        }
+        else{
+            count=1;
+            listaMetodi.put(nomeMetodo, count);
+        }
+        
+        System.out.println("Nome metodo: " + nomeMetodo + ", chiamato " + count + " volte.");
+        
+        return ic.proceed();
+    }
+    ***************************************************************************/
+
 }
